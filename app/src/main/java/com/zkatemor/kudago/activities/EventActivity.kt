aebe0185006387c04ab32225f22049d6f6f8f43d
zkatemor <rinaholmes7@gmail.com>
2019-03-10 01:39:30 +0300

    added google map

--- app/src/main/java/com/zkatemor/kudago/activities/EventActivity.kt
+++ app/src/main/java/com/zkatemor/kudago/activities/EventActivity.kt
@@ -3,13 +3,39 @@ package com.zkatemor.kudago.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.zkatemor.kudago.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class EventActivity : AppCompatActivity() {
+
+class EventActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
+
+        createMapView()
+    }
+
+    private fun createMapView() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map_view) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     fun onClickArrow(v : View){
