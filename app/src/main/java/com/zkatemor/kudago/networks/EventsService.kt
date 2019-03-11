b47f00c06c1e1c78390ca7b593632b907947a875
zkatemor <rinaholmes7@gmail.com>
2019-03-11 22:47:07 +0300

    fixed description textview on event card

--- app/src/main/java/com/zkatemor/kudago/networks/EventsService.kt
+++ app/src/main/java/com/zkatemor/kudago/networks/EventsService.kt
@@ -6,6 +6,9 @@ import retrofit2.http.GET
 const val BASE_URL = "https://kudago.com/public-api/v1.4/"
 
 interface EventsService {
-    @GET("events/?fields=id,dates,title,place,price,description,images,body_text&expand=place,dates")
+    @GET(
+        "events/?fields=id,dates,title,place,price,description,images,body_text&expand=place,dates"
+                + "&order_by=-publication_date&text_format=text"
+    )
     fun getEvents(): Call<EventsResponse>
 }
